8aec1d3f78d14680d9b53ad4509fb1f80e30f54c
Fred Colomb <fred@silvershadowstudio.com>
2026-05-19 00:59:33 +0100

    Invite email subject: drop period. Agreement email: remove silvershadowstudio.com footer

--- supabase/functions/_shared/emailTemplates.ts
+++ supabase/functions/_shared/emailTemplates.ts
@@ -2,7 +2,7 @@ const LOGO_URL =
   'https://silvershadowstudio.s3.eu-central-1.amazonaws.com/Silvershadow/SilvershadowStudio.png'
 
 export const EMAIL_INVITE_DEFAULTS = {
-  subject: 'Your portal is ready.',
+  subject: 'Your portal is ready',
   illustrationUrl:
     'https://silvershadowstudio.s3.eu-central-1.amazonaws.com/Silvershadow/APP+Files/portal-invite-illustration.png',
   bodyCopy: 'Your Silver Shadow Studio portal is now ready.<br>You will find your projects, deliveries,<br>and studio correspondence within.',
